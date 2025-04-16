3dd4ae16f0d6ebc54ed3133068f57945ae321ae8
JBC <joci1358@student.su.se>
2025-04-16 11:09:05 +0200

    New file

---
+++ test2.r
@@ -0,0 +1,22 @@
+
+##create R terminal
+
+# myfunc(x,y) return largest number of v1 and v2
+myfunc = function(v1,v2) max(v1,v2)
+
+dat = data.frame(a = c(1,2), 
+b = c(3,4))
+
+# Gives you infromation about the code
+?c
+
+## Print data in the terminal
+dat
+
+1 == 2
+1 < 2
+1 == 2 & 1 < 2
+1 == 2 | 1 < 2
+
+
+
